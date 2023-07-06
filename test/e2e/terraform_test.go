5feeed58dfcc38332c662493a00929da87252c0f
luke-taylor <lukejamestaylor98@outlook.com>
2023-07-06 14:59:46 +0100

    format

--- test/e2e/terraform_test.go
+++ test/e2e/terraform_test.go
@@ -23,3 +23,18 @@ func TestExamplesBasic(t *testing.T) {
 		assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.Automation/automationAccounts/.+"), output["test_automation_account_resource_id"])
 	})
 }
+
+func TestExamplesComplete(t *testing.T) {
+	test_helper.RunE2ETest(t, "../../", "examples/complete", terraform.Options{
+		Upgrade: true,
+	}, func(t *testing.T, output test_helper.TerraformOutput) {
+		logAnalyticsId, ok := output["test_log_analytics_workspace_resource_id"].(string)
+		assert.True(t, ok)
+		assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.OperationalInsights/workspaces/.+"), logAnalyticsId)
+		assert.True(t, ok)
+		assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.Automation/automationAccounts/.+"), output["test_automation_account_resource_id"])
+		automationAccountMsiId, ok := output["test_automation_account_msi_resource_id"].(string)
+		assert.True(t, ok)
+		assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.Automation/automationAccounts/.+/providers/Microsoft.ManagedIdentity/.+/.+"), automationAccountMsiId)
+	})
+}
